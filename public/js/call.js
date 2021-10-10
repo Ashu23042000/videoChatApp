6b02f2665577406cf95ab0179b718a11841939f1
Ashu <ashu@gmail.com>
2021-10-10 18:22:43 +0530

    some changes

--- public/js/call.js
+++ public/js/call.js
@@ -6,107 +6,122 @@ const hamburger_navMenu = document.querySelector(".hamburger_navMenu");
 hamburger.addEventListener("click", () => {
     hamburger_navMenu.classList.toggle("show");
 });
-console.log(roomId);
-
-
-const peer = new Peer()
-const stream_section = document.querySelector(".stream_section");
-const myVideo = document.createElement("video");
-myVideo.muted = true;
-myVideo.classList.add("myStream");
-// myVideo.width = 100;
-// myVideo.height = 200;
+// console.log(roomId);
 
 
 
+if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
 
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
-        video.play();
-    });
-    stream_section.appendChild(video);
-};
+
+
+    const peer = new Peer()
+    const stream_section = document.querySelector(".stream_section");
+    const myVideo = document.createElement("video");
+    myVideo.muted = true;
+    myVideo.classList.add("myStream");
+    // myVideo.width = 100;
+    // myVideo.height = 200;
 
 
 
 
-function connectToOtherUser(userId, stream) {
-    const call = peer.call(userId, stream);
-    const video = document.createElement("video");
-    // video.height = 200;
-    // video.width = 100;
-    video.classList.add("otherStream");
-    call.on("stream", (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
-    });
-    call.on("close", () => {
-        video.remove();
-    });
-};
+    function addVideoStream(video, stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+            video.play();
+        });
+        stream_section.appendChild(video);
+    };
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then((stream) => {
 
-    addVideoStream(myVideo, stream);
-    peer.on("call", (call) => {
-        call.answer(stream);
+
+
+    function connectToOtherUser(userId, stream) {
+        const call = peer.call(userId, stream);
         const video = document.createElement("video");
+        // video.height = 200;
+        // video.width = 100;
         video.classList.add("otherStream");
         call.on("stream", (userVideoStream) => {
-            // video.height = 200;
-            // video.width = 100;
             addVideoStream(video, userVideoStream);
         });
+        call.on("close", () => {
+            video.remove();
+        });
+    };
+
+    navigator.mediaDevices.getUserMedia({
+        // video: true,
+        video: {
+            facingMode: 'user'
+        },
+        audio: true
+    }).then((stream) => {
+        addVideoStream(myVideo, stream);
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            video.classList.add("otherStream");
+            call.on("stream", (userVideoStream) => {
+                // video.height = 200;
+                // video.width = 100;
+                addVideoStream(video, userVideoStream);
+            });
+        });
+
+
+        // when first user connect to other user---
+        socket.on("user_connected", (userId) => {
+            // calling other client and sending stream----
+            connectToOtherUser(userId, stream);
+        });
+
+
+        // video audio controls---------------
+        const mikeon = document.querySelector(".mikeon");
+        const mikeoff = document.querySelector(".mikeoff");
+        const videoon = document.querySelector(".videoon");
+        const videooff = document.querySelector(".videooff");
+
+
+        mikeon.addEventListener("click", () => {
+            stream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
+            mikeon.style.display = "none";
+            mikeoff.style.display = "block";
+
+        });
+        mikeoff.addEventListener("click", () => {
+            stream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
+            mikeon.style.display = "block";
+            mikeoff.style.display = "none";
+        });
+
+        videoon.addEventListener("click", () => {
+            stream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+            videoon.style.display = "none";
+            videooff.style.display = "block";
+
+        });
+
+        videooff.addEventListener("click", () => {
+            stream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+            videoon.style.display = "block";
+            videooff.style.display = "none";
+        });
+
+    }).catch((err) => {
+        console.log(err);
     });
 
 
-    // when first user connect to other user---
-    socket.on("user_connected", (userId) => {
-        // calling other client and sending stream----
-        connectToOtherUser(userId, stream);
+    peer.on("open", (id) => {
+        socket.emit("join_room", roomId, id);
     });
 
 
-    // video audio controls---------------
-    const mikeon = document.querySelector(".mikeon");
-    const mikeoff = document.querySelector(".mikeoff");
-    const videoon = document.querySelector(".videoon");
-    const videooff = document.querySelector(".videooff");
+}
+else {
+    alert("media device not supported");
+}
 
 
-    mikeon.addEventListener("click", () => {
-        stream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
-        mikeon.style.display = "none";
-        mikeoff.style.display = "block";
-
-    });
-    mikeoff.addEventListener("click", () => {
-        stream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
-        mikeon.style.display = "block";
-        mikeoff.style.display = "none";
-    });
-
-    videoon.addEventListener("click", () => {
-        stream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
-        videoon.style.display = "none";
-        videooff.style.display = "block";
-
-    });
-
-    videooff.addEventListener("click", () => {
-        stream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
-        videoon.style.display = "block";
-        videooff.style.display = "none";
-    });
-
-}).catch((err) => {
-    console.log(err);
-});
-
-
-peer.on("open", (id) => {
-    socket.emit("join_room", roomId, id);
-});
